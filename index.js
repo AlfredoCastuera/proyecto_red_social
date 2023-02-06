ea2bcc694b7f56c6481e6950181eaeada73e0c6a
Freddy castuera <freddycastuera@gmail.com>
2023-02-05 18:03:25 -0700

    creamos los ficheros necesarios para comenzar el proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res)=>{
+  res.send('hola mundo')
+})
+
+app.listen(3000, ()=> console.log('estoy escuchando en el puerto 3000'));

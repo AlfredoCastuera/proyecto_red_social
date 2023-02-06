7f1d85b9b4edfc83652d747da6b420eb9cfdb457
Freddy castuera <freddycastuera@gmail.com>
2023-02-05 19:30:13 -0700

    Agregue el router de usuario

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express';
 import connect from './config/database_conection.js';
+import { router as userRouter } from './routes/Users.Router.js'
+console.log(userRouter);
 
 try {
   await connect(process.env.DATABASE_URI);
@@ -10,6 +12,8 @@ try {
 }
 
 const app = express();
+
+app.use('/users', userRouter);
 
 app.get('/', (req,res)=>{
   res.send('hola mundo')
